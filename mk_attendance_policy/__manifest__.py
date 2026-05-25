8b4f7415d44e5de2be19e3ac4c5db59affc9444c
mohamedkandil <m.abdallah_kandil@hotmail.com>
2026-05-25 15:56:28 +0300

    bannar

--- mk_attendance_policy/__manifest__.py
+++ mk_attendance_policy/__manifest__.py
@@ -5,6 +5,7 @@
     'website': "mohamed.kandil@myntrocode.com",
     'price': 100,
     'currency': 'USD',
+    'summary': 'Attendance policy rules for late deductions, overtime, absence, public holidays, and payroll inputs.',
     'depends': [
         'hr',
         'hr_attendance',
@@ -19,6 +20,10 @@
         'views/mk_attendance_views.xml',
         'views/hr_employee_views.xml',
         'views/attendance_sheet_line_views.xml',
+    ],
+    'images': [
+        'static/description/banner.png',
+        'static/description/icon.png',
     ],
     'installable': True,
     'application': True,
