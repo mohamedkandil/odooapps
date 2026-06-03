ec6456ef2b978144ed9647e69dfd0042a7715fd4
mohamedkandil <m.abdallah_kandil@hotmail.com>
2026-06-03 09:35:47 +0300

    v1

--- mk_ccess_manament/__manifest__.py
+++ mk_ccess_manament/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "Administration",
     "author": "Kandil ERP",
     "license": "LGPL-3",
-    'price': 399.00,
+    'price': 350.00,
     'currency': 'USD',
     "web_icon": "mk_ccess_manament,static/description/icon.png",
     "images": [
